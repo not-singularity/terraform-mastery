7407c02fe28dac94bc89d331f1335ffcded0713f
not-singularity <155046782+not-singularity@users.noreply.github.com>
2023-12-30 05:54:53 +0000

    add

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -41,7 +41,7 @@ resource "aws_route_table" "rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
