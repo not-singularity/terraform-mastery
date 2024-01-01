709898329a01cdcd7f573743e08f6c46841e5afc
not-singularity <155046782+not-singularity@users.noreply.github.com>
2024-01-01 11:40:50 +0000

    add

--- modules/statelock/main.tf
+++ modules/statelock/main.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "terraform-statelock" {
-  name = "terraform-lock"
+resource "aws_dynamodb_table" "tf-statelock" {
+  name = "terraform-statelock"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
 
--- staging/backend.tf
+++ staging/backend.tf
@@ -1,7 +1,7 @@
 # terraform {
 #   backend "s3" {
 #     bucket  = "tf-remote-statefile"
-#     key     = "dev/terraform.tfstate"
+#     key     = "staging/terraform.tfstate"
 #     region  = "ap-south-1"
 #     encrypt = true
 #     # dynamodb_table = "terraform-statelock"
